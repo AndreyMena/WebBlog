303d655975c8fc4d6b8539f70cb82a03892ed160
Andrey Mena Espinoza <andrey.menaespinoza@ucr.ac.cr>
2023-06-04 11:35:40 -0600

    Delete confirmation in register user and conect roles identity

---
+++ WebBlog/Controllers/RolesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebBlog.Controllers
+{
+    public class RolesController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public RolesController(RoleManager<IdentityRole> _roleManager) 
+        {
+            roleManager = _roleManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
