43ca8669aa53c7d17ad6a237146db0977e43dfff
Sam Cheang <sam.cheang.h@gmail.com>
2023-06-10 22:21:21 -0600

    Admin Posts completed

--- WebBlog/Controllers/AdminBlogPostsController.cs
+++ WebBlog/Controllers/AdminBlogPostsController.cs
@@ -72,5 +72,87 @@ namespace WebBlog.Controllers
             var blogPost = await _postRepository.GetAllAsync();
             return View(blogPost);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var blogPost = await _postRepository.GetAsync(id);
+            var tagsDomainModel = await _tagRepository.GetAllAsync();
+
+            if (blogPost != null)
+            {
+                var model = new EditBlogPostRequest
+                {
+                    Id = blogPost.Id,
+                    Heading = blogPost.Heading,
+                    PageTitle = blogPost.PageTitle,
+                    Content = blogPost.Content,
+                    ShortDescription = blogPost.ShortDescription,
+                    ImageUrl = blogPost.ImageUrl,
+                    UrlHandle = blogPost.UrlHandle,
+                    PublishedDate = blogPost.PublishedDate,
+                    Author = blogPost.Author,
+                    Visible = blogPost.Visible,
+                    Tags = tagsDomainModel.Select(x => new SelectListItem { Text = x.Name, Value = x.Id.ToString() }),
+                    SelectedTags = blogPost.Tags.Select(x => x.Id.ToString()).ToArray()
+                };
+                return View(model);
+            }
+            return View(null);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditBlogPostRequest editBlogPostRequest)
+        {
+            var blogPostDomainModel = new BlogPost
+            {
+                Id = editBlogPostRequest.Id,
+                Heading = editBlogPostRequest.Heading,
+                PageTitle = editBlogPostRequest.PageTitle,
+                Content = editBlogPostRequest.Content,
+                ShortDescription = editBlogPostRequest.ShortDescription,
+                ImageUrl = editBlogPostRequest.ImageUrl,
+                UrlHandle = editBlogPostRequest.UrlHandle,
+                PublishedDate = editBlogPostRequest.PublishedDate,
+                Author = editBlogPostRequest.Author,
+                Visible = editBlogPostRequest.Visible,
+            };
+
+            var selectedTags = new List<Tag>();
+            foreach (var selectedTag in editBlogPostRequest.SelectedTags)
+            {
+                if (Guid.TryParse(selectedTag, out var tag))
+                {
+                    var foundTag = await _tagRepository.GetAsync(tag);
+                    if (foundTag != null)
+                    {
+                        selectedTags.Add(foundTag);
+                    }
+                }
+            }
+
+            blogPostDomainModel.Tags = selectedTags;
+
+            var updatedPost = await _postRepository.UpdateAsync(blogPostDomainModel);
+
+            if (updatedPost != null)
+            {
+                return RedirectToAction("List");
+            }
+
+            return RedirectToAction("Edit");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(EditBlogPostRequest editBlogPostRequest)
+        {
+            var deletedBlogPost = await _postRepository.DeleteAsync(editBlogPostRequest.Id);
+
+            if (deletedBlogPost != null)
+            {
+                return RedirectToAction("List");
+            }
+            return RedirectToAction("Edit", new { id = editBlogPostRequest.Id });
+        }
     }
 }
--- WebBlog/Repositories/BlogPostRepository.cs
+++ WebBlog/Repositories/BlogPostRepository.cs
@@ -22,22 +22,48 @@ namespace WebBlog.Repositories
 
         public async Task<BlogPost> DeleteAsync(Guid id)
         {
-            throw new NotImplementedException();
+            var existingBlog = await _context.BlogPost.FindAsync(id);
+            if (existingBlog != null)
+            {
+                _context.BlogPost.Remove(existingBlog);
+                await _context.SaveChangesAsync();
+                return existingBlog;
+            }
+            return null;
         }
 
         public async Task<IEnumerable<BlogPost>> GetAllAsync()
         {
-            return await _context.BlogPost.ToListAsync();
+            return await _context.BlogPost.Include(x => x.Tags).ToListAsync();
         }
 
         public async Task<BlogPost> GetAsync(Guid id)
         {
-            throw new NotImplementedException();
+            return await _context.BlogPost.Include(x => x.Tags).FirstOrDefaultAsync(x => x.Id == id);
         }
 
         public async Task<BlogPost> UpdateAsync(BlogPost blogPost)
         {
-            throw new NotImplementedException();
+            var existingBlog = await _context.BlogPost.Include(x => x.Tags).FirstOrDefaultAsync(x => x.Id == blogPost.Id);
+
+            if (existingBlog != null)
+            {
+                existingBlog.Id = blogPost.Id;
+                existingBlog.Heading = blogPost.Heading;
+                existingBlog.PageTitle = blogPost.PageTitle;
+                existingBlog.Content = blogPost.Content;
+                existingBlog.ShortDescription = blogPost.ShortDescription;
+                existingBlog.Author = blogPost.Author;
+                existingBlog.ImageUrl = blogPost.ImageUrl;
+                existingBlog.UrlHandle = blogPost.UrlHandle;
+                existingBlog.Visible = blogPost.Visible;
+                existingBlog.PublishedDate = blogPost.PublishedDate;
+                existingBlog.Tags = blogPost.Tags;
+
+                await _context.SaveChangesAsync();
+                return existingBlog;
+            }
+            return null;
         }
     }
 }
