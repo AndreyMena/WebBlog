ad1f574e52e879d5ed56693157f34b7b94b2d099
Andrey Mena Espinoza <andrey.menaespinoza@ucr.ac.cr>
2023-06-11 16:24:34 -0600

    Register view with controller methods

---
+++ WebBlog/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebBlog.Models;
+using WebBlog.Models.Domain;
+using WebBlog.Models.ViewModels;
+
+namespace WebBlog.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<AppUser> signInManager;
+        private readonly UserManager<AppUser> userManager;
+
+        public AccountController(
+            UserManager<AppUser> _userManager,
+            SignInManager<AppUser> _signInManager
+            )
+        {
+            userManager = _userManager;
+            signInManager = _signInManager;
+        }
+
+        [BindProperty]
+        public RegisterInputModel Input { get; set; }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(UserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new AppUser { UserName = model.Email, Email = model.Email, FirstName = model.FirstName, LastName = model.LastName };
+                var result = await userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("index", "home");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View();
+        }
+    }
+}
