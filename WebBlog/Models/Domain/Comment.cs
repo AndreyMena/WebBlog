a8c21022c99019e4fc1e6dc45d3845a692e6a089
Sam Cheang <sam.cheang.h@gmail.com>
2023-06-14 20:37:44 -0600

    200 caracteres max length comment

--- WebBlog/Models/Domain/Comment.cs
+++ WebBlog/Models/Domain/Comment.cs
@@ -1,9 +1,13 @@
-﻿namespace WebBlog.Models.Domain
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebBlog.Models.Domain
 {
     public class Comment
     {
         public Guid Id { get; set; }
         public string Email { get; set; }
+
+        [MaxLength(200)]
         public string Description { get; set; }
         public DateTime DateAdded { get; set; }
         public Guid PostId { get; set; }
